0730a46275bb6922def73021613bf42761203038
Huzayiru Kalungi <169085666+hwylhuzail@users.noreply.github.com>
2026-09-18 21:49:59 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  // Only export static when building APK
   ...(process.env.EXPORT === "true" ? { output: "export", images: { unoptimized: true } } : {}),
 };
-
 module.exports = nextConfig;
