899f8079ee54de84225219c734a2f1098703df79
Huzayiru Kalungi <169085666+hwylhuzail@users.noreply.github.com>
2026-09-18 20:34:56 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isExport = process.env.EXPORT === 'true'
+
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
+ ...(isExport? { output: 'export', distDir: 'out', trailingSlash: true } : {}),
   images: { unoptimized: true },
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  trailingSlash: true,
 }
+
 module.exports = nextConfig
