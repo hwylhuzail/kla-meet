cd5aa884191aea13b950240ec3fbb46de7d706ef
Huzayiru Kalungi <169085666+hwylhuzail@users.noreply.github.com>
2026-09-19 10:35:23 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  distDir: 'out',
-  images: { unoptimized: true },
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  trailingSlash: true,
-}
-module.exports = nextConfig
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.klameet.app',
+  appName: 'kla-meet',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
