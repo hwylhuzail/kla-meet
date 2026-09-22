c3d9c70a247f6c2abef4128ba3427e9f36220587
Huzayiru Kalungi <169085666+hwylhuzail@users.noreply.github.com>
2026-09-22 09:21:19 +0300

    Update LayoutClient.tsx

--- components/LayoutClient.tsx
+++ components/LayoutClient.tsx
@@ -5,14 +5,21 @@ import BottomNav from '@/components/BottomNav';
 
 export default function LayoutClient({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const isHome = pathname === '/'; // hide global nav on home only
+
+  // hide global nav on these routes
+  const hideNav =
+    pathname === '/' ||
+    pathname.startsWith('/chat') ||
+    pathname.startsWith('/auth') ||
+    pathname.startsWith('/verification');
+
   return (
     <>
-      {!isHome && <Header />}
-      <main className={isHome? "" : "pb-[80px] max-w-md mx-auto"}>
+      {!hideNav && <Header />}
+      <main className={hideNav? "" : "pb-[90px] max-w-md mx-auto min-h-screen bg-[#fbf9ff] dark:bg-black"}>
         {children}
       </main>
-      {!isHome && <BottomNav />}
+      {!hideNav && <BottomNav />}
     </>
   );
 }
