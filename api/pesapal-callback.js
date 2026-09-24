b0212eec33bb0ba6f8ccdc16707b4ddd787ed393
Huzayiru Kalungi <169085666+hwylhuzail@users.noreply.github.com>
2026-09-24 12:25:32 +0300

    Update pesapal-callback.js

--- api/pesapal-callback.js
+++ api/pesapal-callback.js
@@ -1,4 +1,4 @@
 export default function handler(req,res){
-  const { order } = req.query
-  return res.redirect(302, `/?premium=success&order=${order}`)
+  const { order, OrderTrackingId } = req.query
+  return res.redirect(302, `/?premium=success&order=${order || OrderTrackingId || 'ok'}`)
 }
