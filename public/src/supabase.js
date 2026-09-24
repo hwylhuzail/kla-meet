a52313ba2b2fcec03a23c5072a8a68254b0b10eb
Huzayiru Kalungi <169085666+hwylhuzail@users.noreply.github.com>
2026-09-24 13:41:00 +0300

    Delete supabase.js

--- public/src/supabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-export const supabase = createClient(
-  'https://YOUR_PROJECT.supabase.co', // <-- replace with your URL
-  'YOUR_ANON_KEY' // <-- replace with your anon key
-)
